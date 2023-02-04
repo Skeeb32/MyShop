1add0590b33233d54d28d8f1c57b523ed766620d
Skeeb32 <skeeb32@gmail.com>
2023-02-04 16:26:48 -0500

    Categories

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Categories from '../../components/Categories/Categories'
 import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts';
 import Slider from '../../components/Slider/Slider';
 import './Home.scss';
@@ -7,6 +8,7 @@ const Home = () => {
     <div className="home">
       <Slider />
       <FeaturedProducts type="featured" />
+      <Categories/>
       <FeaturedProducts type="trending" />
     </div>
   );

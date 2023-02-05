d3967569463733823ec71f733afe212cde88ca3f
Skeeb32 <skeeb32@gmail.com>
2023-02-05 15:52:42 -0500

    Cart

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from 'react';
+import './Cart.scss';
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 
-function Cart() {
+const Cart = () => {
+  const data = [
+    {
+      id: 1,
+      img: 'https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      img2: 'https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      title: 'Long Sleve Graphic T-Shirt',
+      desc: "Anything",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 2,
+      img: 'https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress&cs=tinysrgb&w=1600',
+      title: 'Coat',
+      desc: "Anything",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+  ];
+
   return (
-    <div>
-      
+    <div className="cart">
+      <h1>Products in your cart</h1>
+      {data?.map((item) => (
+        <div className="item" key={item.id}>
+          <img src={item.img} alt="" />
+          <div className="details">
+            <h1>{item.title}</h1>
+            <p>{item.desc?.substring(0, 100)}</p>
+            <div className="price">1 x ${item.price}</div>
+          </div>
+          <DeleteOutlinedIcon className="delete" />
+        </div>
+      ))}
+      <div className="total">
+      <span>SUBTOTAL</span>
+      <span>$123</span>
     </div>
-  )
-}
-
-export default Cart
+    <button>PROCEED TO CHECKOUT</button>
+    <span className="reset">Reset Cart</span>
+    </div>
+  );
+};
+export default Cart;

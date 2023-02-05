88476f233b28e240ae4d06012b07cc3f79448a6e
Skeeb32 <skeeb32@gmail.com>
2023-02-05 14:25:56 -0500

    Products

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { useState } from "react";
-import { useParams } from "react-router-dom";
-import List from "../../components/List/List";
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import List from '../../components/List/List';
 import './Products.scss';
 
 const Products = () => {
-
   const catId = parseInt(useParams().id);
   const [maxPrice, setMaxPrice] = useState(1000);
   const [sort, setSort] = useState(null);
@@ -33,17 +32,34 @@ const Products = () => {
           <h2>Filter by price</h2>
           <div className="inputItem"></div>
           <span>0</span>
-          <input type="range" min={0} max={1000} />
-          <span>1000</span>
+          <input
+            type="range"
+            min={0}
+            max={1000}
+            onChange={(e) => setMaxPrice(e.target.value)}
+          />
+          <span>{maxPrice}</span>
         </div>
         <div className="filterItem">
           <h2>Sort by</h2>
           <div className="inputItem">
-            <input type="radio" id="asc" value="asc" name="price" />
-            <label htmlFor="asc">Price (Lowest firs)</label>
+            <input
+              type="radio"
+              id="asc"
+              value="asc"
+              name="price"
+              onChange={(e) => setSort('asc')}
+            />
+            <label htmlFor="asc">Price (Lowest first)</label>
           </div>
           <div className="inputItem">
-            <input type="radio" id="asc" value="asc" name="price" />
+            <input
+              type="radio"
+              id="asc"
+              value="asc"
+              name="price"
+              onChange={(e) => setSort('desc')}
+            />
             <label htmlFor="asc">Price (Highest first)</label>
           </div>
         </div>
@@ -55,6 +71,10 @@ const Products = () => {
           alt=""
         />
         <List
+          catId={catId}
+          maxPrice={maxPrice}
+          sort={sort}
+          subCats={selectedSubCats}
         />
       </div>
     </div>
